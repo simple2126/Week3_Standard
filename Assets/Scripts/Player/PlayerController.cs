f63dd42fe2032815dafe83ac4cc5b45b71142752
simple2126 <kimcw212@gmail.com>
2024-10-31 11:09:19 +0900

    점프 구현 완료

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -8,6 +8,17 @@ public class PlayerController : MonoBehaviour
     public float moveSpeed;
     private Vector2 curInput;
     private Rigidbody rb;
+    public float jumpForce;
+    public LayerMask groundMask;
+
+    [Header("Look")]
+    public Transform cameraContainer;
+    public float lookSensitivity;
+    public float maxLookAngle;
+    public float minLookAngle;
+    private bool canLook = true;
+    private Vector2 mouseDelta;
+    private float cameraXRot;
 
     private void Awake()
     {
@@ -22,6 +33,14 @@ public class PlayerController : MonoBehaviour
     void FixedUpdate()
     {
         Move();
+    }
+
+    void LateUpdate()
+    {
+        if (canLook)
+        {
+            Look();
+        }
     }
 
     public void OnMove(InputAction.CallbackContext context)
@@ -41,5 +60,46 @@ public class PlayerController : MonoBehaviour
         newPos *= moveSpeed;
         newPos.y = rb.velocity.y;
         rb.velocity = newPos;
+    }
+
+    public void OnLook(InputAction.CallbackContext context)
+    {
+        mouseDelta = context.ReadValue<Vector2>();
+    }
+
+    private void Look()
+    {
+        cameraXRot += mouseDelta.y * lookSensitivity;
+        cameraXRot = Mathf.Clamp(cameraXRot, minLookAngle, maxLookAngle);
+        cameraContainer.localEulerAngles = new Vector3(-cameraXRot, 0, 0);
+        transform.eulerAngles += new Vector3(0, mouseDelta.x * lookSensitivity, 0);
+    }
+
+    public void OnJump(InputAction.CallbackContext context)
+    {
+        if (context.phase == InputActionPhase.Started && IsGround())
+        {
+            rb.AddForce(Vector2.up * jumpForce, ForceMode.Impulse);
+        }
+    }
+
+    private bool IsGround()
+    {
+        Ray[] rays = new Ray[4]
+        {
+            new Ray(transform.position + (transform.forward * 0.2f) + (transform.up * 0.01f), Vector3.down),
+            new Ray(transform.position + (-transform.forward * 0.2f) + (transform.up * 0.01f), Vector3.down),
+            new Ray(transform.position + (transform.right * 0.2f) + (transform.up * 0.01f), Vector3.down),
+            new Ray(transform.position + (-transform.right * 0.2f) +(transform.up * 0.01f), Vector3.down)
+        };
+
+        for (int i = 0; i < rays.Length; i++)
+        {
+            if (Physics.Raycast(rays[i], 0.1f, groundMask))
+            {
+                return true;
+            }
+        }
+        return false;
     }
 }
